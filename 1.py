77c61733d764090b5abe02e870a23c41088e4efb
hugogarciaarocas <61227490+hugogarciaarocas@users.noreply.github.com>
2020-02-24 19:27:26 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,41 @@
+print("ejercicio 1:")
+x = 4 + 5
+print(x)
+
+print("ejercicio 2:")
+x = 2
+y = 3.4
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+
+print("ejercicio 3:")
+b1 = True
+b2 = False
+print(b1 and b2)
+print(b1 or b2)
+print((b1 and b2) or (b1 or b2))
+print(not b1)
+
+print("ejercicio 4:")
+
+print("convertidor de grados Celsius a Fahrenheit")
+x = int(input ("introduzca grados Celsius: "))
+y = (((x * 9) / 5) + 32)
+print(str(x) + " grados Celsius son: " + str(y) + " grados Fahrenheit")
+
+print("Convertidor de metros en millas")
+x = int(input("Introduzca metros: "))
+y = (x * 0.00062137)
+print(str(x) + " metros son: " + str(y) + " millas.")
+
+
+
+
+
+
+
+
+
